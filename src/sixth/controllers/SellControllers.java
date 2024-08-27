daf0dbd74467fb5dc5768b36df2d994a85f8a576
parnian <parnian.ghn83@gmail.com>
2024-08-27 13:06:26 +0330

    SellDa

--- src/sixth/controllers/SellControllers.java
+++ src/sixth/controllers/SellControllers.java
@@ -1,5 +1,48 @@
 package sixth.controllers;
 
-public class SellControllers {
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import sixth.model.da.ProductDa;
+import sixth.model.entity.Brand;
+import sixth.model.entity.Product;
+
+import java.net.URL;
+import java.sql.Timestamp;
+import java.util.List;
+import java.util.ResourceBundle;
+
+public class SellControllers  implements Initializable {
+    @FXML
+    private TextField idTxt, nameTxt, familyTxt, productTxt;
+    @FXML
+    private DatePicker sellTimePicker;
+    @FXML
+    private TableView<Product> mainTable;
+    @FXML
+    private TableColumn<Product, Integer> idCol;
+    @FXML
+    private TableColumn<Product, String> nameCol;
+    @FXML
+    private TableColumn<Product, String> familyCol;
+    @FXML
+    private TableColumn<Product, String> productCol;
+    @FXML
+    private TableColumn<Product, Timestamp> sellTimeCol;
+    @FXML
+    private Button saveBtn, editBtn, removeBtn;
+
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+
 
 }
+
+
+
--- src/sixth/model/da/SellDa.java
+++ src/sixth/model/da/SellDa.java
@@ -2,15 +2,14 @@ package sixth.model.da;
 
 
 
-import sixth.model.entity.Person;
-import sixth.model.entity.Sell;
+import sixth.model.entity.*;
 import sixth.model.utils.JdbcProvider;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -23,28 +22,120 @@ public class SellDa implements AutoCloseable {
     }
 
     public void save(Sell sell) throws SQLException {
-        preparedStatement = connection.prepareStatement(
-                "SELECT SELL_SEQ.NEXTVAL AS NEXT_ID FROM DUAL"
-        );
-        ResultSet resultSet = preparedStatement.executeQuery();
-        resultSet.next();
-        sell.setId(resultSet.getInt("NEXT_ID"));
+        try {
+            preparedStatement = connection.prepareStatement(
+                    "SELECT SELL_SEQ.NEXTVAL AS NEXT_ID FROM DUAL"
+            );
+            ResultSet resultSet = preparedStatement.executeQuery();
+            resultSet.next();
+            sell.setId(resultSet.getInt("NEXT_ID"));
 
-        preparedStatement = connection.prepareStatement(
-                "INSERT INTO SELL VALUES (?,?,?,?,?)"
-        );
-        preparedStatement.setInt(1, sell.getId());
-        preparedStatement.setString(2, sell.getPerson().getName());
-        preparedStatement.setString(3, sell.getPerson().getFamily());
-        preparedStatement.setString(4, sell.getProduct().getName());
+            preparedStatement = connection.prepareStatement(
+                    "INSERT INTO SELL (ID, PRICE, SELL_TIME, OWNER_ID, PRODUCT_ID) VALUES (?, ?, ?, ?, ?)"
+            );
+            preparedStatement.setInt(1, sell.getId());
+            preparedStatement.setInt(2, sell.getPrice());
+            preparedStatement.setString(3, sell.getSellTime().toString());
+            preparedStatement.setInt(4, sell.getPerson().getId());
+            preparedStatement.setInt(5, sell.getProduct().getId());
 
-        preparedStatement.execute();
+            preparedStatement.executeUpdate();
+        } finally {
+            if (preparedStatement != null) {
+                preparedStatement.close();
+            }
+        }
     }
 
+    public void edit(Sell sell) throws SQLException {
+        try {
+            preparedStatement = connection.prepareStatement(
+                    "UPDATE SELL SET PRICE=?, SELL_TIME=?, OWNER_ID=?, PRODUCT_ID=? WHERE ID=?"
+            );
 
+            preparedStatement.setInt(1, sell.getPrice());
+            preparedStatement.setString(2, sell.getSellTime().toString());
+            preparedStatement.setInt(3, sell.getPerson().getId());
+            preparedStatement.setInt(4, sell.getProduct().getId());
+            preparedStatement.setInt(5, sell.getId());
+
+            preparedStatement.executeUpdate();
+        } finally {
+            if (preparedStatement != null) {
+                preparedStatement.close();
+            }
+        }
+    }
+
+    public void remove(int id) throws SQLException {
+        try {
+            preparedStatement = connection.prepareStatement(
+                    "DELETE FROM SELL WHERE ID=?"
+            );
+            preparedStatement.setInt(1, id);
+            preparedStatement.executeUpdate();
+        } finally {
+            if (preparedStatement != null) {
+                preparedStatement.close();
+            }
+        }
+    }
+
+    public List<Sell> findAll() throws SQLException {
+        List<Sell> sellList = new ArrayList<>();
+        try {
+            preparedStatement = connection.prepareStatement(
+                    "SELECT * FROM SELL ORDER BY ID"
+            );
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+            while (resultSet.next()) {
+                Sell sell = Sell.builder()
+                        .id(resultSet.getInt("ID"))
+                        .price(resultSet.getInt("PRICE"))
+                        .sellTime(LocalDateTime.parse(resultSet.getString("SELL_TIME")))
+                    //    .person(new Person(resultSet.getInt("OWNER_ID")))
+                    //    .product(new Product(resultSet.getInt("PRODUCT_ID")))
+                        .build();
+                sellList.add(sell);
+            }
+        } finally {
+            if (preparedStatement != null) {
+                preparedStatement.close();
+            }
+        }
+        return sellList;
+    }
+
+    public Sell findById(Integer id) throws SQLException {
+        Sell sell = null;
+        try {
+            preparedStatement = connection.prepareStatement(
+                    "SELECT * FROM SELL WHERE ID=?"
+            );
+            preparedStatement.setInt(1, id);
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+            if (resultSet.next()) {
+                sell = Sell.builder()
+                        .id(resultSet.getInt("ID"))
+                        .price(resultSet.getInt("PRICE"))
+                        .sellTime(LocalDateTime.parse(resultSet.getString("SELL_TIME")))
+                //        .person(new Person(resultSet.getInt("OWNER_ID")))
+                //        .product(new Product(resultSet.getInt("PRODUCT_ID")))
+                        .build();
+            }
+        } finally {
+            if (preparedStatement != null) {
+                preparedStatement.close();
+            }
+        }
+        return sell;
+    }
 
     @Override
     public void close() throws Exception {
-
+        preparedStatement.close();
+        connection.close();
     }
 }
